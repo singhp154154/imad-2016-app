575706be886bf68aa574845266ea39f7588823de
singhp154154 <singhp154154@gmail.com>
2016-10-03 14:30:29 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/article-one', function(req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two', function(req, res) {
-    res.sendFile(path.join(__dirname, 'iu', 'article-two.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/article-three', function(req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-three'));
