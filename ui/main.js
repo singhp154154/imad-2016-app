a89621371d400746ddba9347d66cb78602e06c3e
singhp154154 <singhp154154@gmail.com>
2016-10-29 10:55:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,6 @@
 console.log('Loaded!');
+//changing the value of html by using inner html tag
+//grab the element
+var eleent = document.getElementById('main-text');
+//apply the change in html by using inner element jvascript
+element.innerHTML = 'hy this timeni have made some changes by the power of jvascript';
