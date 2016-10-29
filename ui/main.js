5662897d259d42c7296ebd3246266d72355c7a7a
singhp154154 <singhp154154@gmail.com>
2016-10-29 11:12:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,15 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 //apply the change in html by using inner element jvascript
 element.innerHTML = 'hy this time i have made some changes by the power of jvascript';
+//moving the image to left gradually
+//create a variable which move element right and initilize it
+var marginright =0;
+//create a function which move the value of the function right gradually
+function moveRight () {
+  marginright = marginright = 1;
+  img.stylr.marginright = marginright + 'px';
+}
+//give the cammand that on click initilise the function 
+img.onclick = function (){
+    var interval = setInterval(moveRight, 50);
+};
